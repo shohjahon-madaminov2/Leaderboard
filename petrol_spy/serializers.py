e47aa850aca4ba3b8ac4d0ad5a47e1153d69d76f
Shohjahon <madaminovsh236@gmail.com>
2026-02-15 16:04:25 +0500

    query optimized

--- petrol_spy/serializers.py
+++ petrol_spy/serializers.py
@@ -1,18 +1,20 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-from rest_framework import serializers
+# from rest_framework import serializers
 
 
-class LeaderborardSerializer(serializers.ModelSerializer):
-    display_name = serializers.SerializerMethodField()
-    reports_count = serializers.SerializerMethodField()
+# class LeaderborardSerializer(serializers.ModelSerializer):
+#     display_name = serializers.SerializerMethodField()
+#     reports_count = serializers.SerializerMethodField()
     
-    class Meta:
-        model = User
-        fields = ('id', 'display_name', 'reports_count')
+#     class Meta:
+#         model = User
+#         fields = ('id', 'display_name', 'reports_count')
         
-    def get_display_name(self, obj):
-        return obj.oneid_profile.full_name if hasattr(obj, 'oneid_profile') and obj.oneid_profile.full_name else obj.username
+        
+#     def get_display_name(self, obj):
+#         return obj.oneid_profile.full_name if hasattr(obj, 'oneid_profile') and obj.oneid_profile.full_name else obj.username
+
             
-    def get_reports_count(self, obj):
-        return obj.reports.count()
+#     def get_reports_count(self, obj):
+#         return obj.reports.count()
--- petrol_spy/views.py
+++ petrol_spy/views.py
@@ -4,17 +4,32 @@ from django.db.models import Count
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from petrol_spy.serializers import LeaderborardSerializer
+from petrol_spy.models import Report
 
 
 class LeaderboardView(APIView):
     def get(self, request):
-        users = User.objects.annotate(
-            reports_count=Count('reports')
-        ).order_by('-reports_count')[:10]
+        top_reports = Report.objects.values('user_id').annotate(
+            cnt = Count('id')
+        ).order_by('-cnt')[:10]
         
-        serializer = LeaderborardSerializer(users, many=True)
+        top_user_ids = [item['user_id'] for item in top_reports]
+        counts_dict = {item['user_id']: item['cnt'] for item in top_reports}
         
-        return Response(
-            {'users': serializer.data}
-        )
+        users = User.objects.filter(
+            id__in=top_user_ids
+        ).select_related('oneid_profile')
+        
+        results = []
+        
+        for user in users:
+            profile = getattr(user, 'oneid_profile', None)
+            results.append(
+                {
+                    'user': user.id,
+                    'display_name': profile.full_name if profile and profile.full_name else user.username,
+                    'reports_count': counts_dict[user.id]
+                }
+            )
+        results.sort(key=lambda x: x['reports_count'], reverse=True)
+        return Response({'users': results})
